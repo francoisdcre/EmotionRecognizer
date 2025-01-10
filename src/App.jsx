d13f5a1dceaf0002f3421bd1703cdee42ab5afed
francoisdcre <francois.dcre@gmail.com>
2025-01-10 15:19:24 +0100

    First Test React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+const title = 'Hello World !'
+
 function App() {
-  return 'Hello World !'
+
+  const hangleCLick = () => {
+    alert("J'ai cliqué sur le titre !")
+  }
+
+  return <>
+    <h1 onClick={hangleCLick} style={{color: 'orange', backgroundColor: 'black'}}>{title}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis vero, asperiores doloribus praesentium ipsam, saepe accusamus doloremque expedita possimus recusandae alias illum facilis omnis. Veniam libero voluptatibus enim expedita porro.</p>
+  </>
 }
 
 export default App
